82d5d8eb7d65494c08809a16545addb0bc10851e
PtSik <patrykgniewek9@gmail.com>
2023-10-10 12:15:44 +0200

    update

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,31 @@
+import { useRef } from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
+    <form className="input" onSubmit={(e) => {
+      handleAdd(e)
+      inputRef.current?.blur()
+      }}>
       <input
+        ref={inputRef}
         type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
         placeholder="Wprowadź zadanie"
         className="input__box"
       />
-      <button className="input_submit" type="submit">Go</button>
+      <button className="input_submit" type="submit">
+        Go
+      </button>
     </form>
   );
 };
